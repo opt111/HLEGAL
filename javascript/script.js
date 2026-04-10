5b98e78abf1e14d7b0baa6a2ba3d1182e4de64ff
Sych Oleh <sicholeg1@gmail.com>
2026-04-10 23:22:15 +0300

    minor changes

--- javascript/script.js
+++ javascript/script.js
@@ -10,20 +10,5 @@ Close.addEventListener('click', function () {
   menu.style.transform = 'translateX(100%)';
 });
 
-const popup = document.getElementById('popup');
-const popupBtn = document.getElementById('popup-btn');
-const entryHeader = document.getElementById('entry-header');
-const popupClose = document.getElementById('popup-close');
 
-popup.addEventListener('click', function () {
-  popup.classList.toggle('popup-active');
-  entryHeader.style.zIndex = '1';
-
-});
-
-
-popupBtn.addEventListener('click', function () {
-  popup.classList.toggle('popup-active');
-  entryHeader.style.zIndex = '0';
-});
 
