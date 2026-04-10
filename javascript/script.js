2d614947a69d2472a4a52619165ac474bf5ec75e
Sych Oleh <sicholeg1@gmail.com>
2026-04-10 22:24:05 +0300

    minor changes

--- javascript/script.js
+++ javascript/script.js
@@ -1,36 +1,29 @@
 const menu = document.querySelector('.header-right__links');
 const burger = document.querySelector('.header-left__burger__div__svg')
-
-
-
 const Close = document.querySelector('.close-icon');
 
+burger.addEventListener('click', function () {
+  menu.style.transform = 'translateX(0%)';
+});
 
-  burger.addEventListener('click', function () {
-    menu.style.transform = 'translateX(0%)';
-  });
-
-  Close.addEventListener('click', function () {
-    menu.style.transform = 'translateX(100%)';
-  });
-
+Close.addEventListener('click', function () {
+  menu.style.transform = 'translateX(100%)';
+});
 
 const popup = document.getElementById('popup');
 const popupBtn = document.getElementById('popup-btn');
 const entryHeader = document.getElementById('entry-header');
 const popupClose = document.getElementById('popup-close');
 
-
-
-
-popup.addEventListener('click', function() {
+popup.addEventListener('click', function () {
   popup.classList.toggle('popup-active');
   entryHeader.style.zIndex = '1';
 
 });
 
 
-popupBtn.addEventListener('click', function() {
+popupBtn.addEventListener('click', function () {
   popup.classList.toggle('popup-active');
   entryHeader.style.zIndex = '0';
 });
+
