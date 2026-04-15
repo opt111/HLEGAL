71cb9e363b0100202bf604a2eca0b3728ef352e7
Sych Oleh <sicholeg1@gmail.com>
2026-04-15 15:33:57 +0300

    minor changes

--- javascript/script.js
+++ javascript/script.js
@@ -1,13 +1,20 @@
 const menu = document.querySelector('.header-right__links');
 const burger = document.querySelector('.header-left__burger__div__svg')
 const Close = document.querySelector('.close-icon');
+const body = document.querySelector('body');
+
+const wraper = document.querySelector('.header-right__links');
 
 burger.addEventListener('click', function () {
   menu.style.transform = 'translateX(0%)';
+  body.classList.add('popup-body-overflow-hidden');
+
 });
+
 
 Close.addEventListener('click', function () {
   menu.style.transform = 'translateX(100%)';
+  body.classList.remove('popup-body-overflow-hidden');
 });
 
 
